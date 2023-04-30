4f91745938cb733f2fb5f781b507b1560c5b3be1
Erica Giselle Francisco Luz <erica.nicollas_hbn@yahoo.com>
2023-04-30 10:29:27 -0300

    Teoria de declarações

---
+++ aulas-js/declaracoes.js
@@ -0,0 +1,11 @@
+let a, b, c;
+a = 5;
+b = 6;
+c = 7;
+
+//ou podemos declara assim:
+let d, e, f; d = 5; e = 6; f = 7;
+
+// ele aceita quebra de linha quando damos enter ele funciona igual
+// ele aceita espaços, pois não lê, então tanto faz.
+
